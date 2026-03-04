0a9672abedc5c0434fd9b39297744b6a8985039e
yaominghao <ymh10@qq.com>
2026-03-04 15:28:04 +0800

    chore: trigger release by modifying src

--- src/subscription.ts
+++ src/subscription.ts
@@ -18,7 +18,7 @@ apps.forEach((appConfig) => {
 export default defineGkdSubscription({
   id: 233,
   name: 'Sober 规则',
-  version: 8,
+  version: 9,
   author: 'Sober',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/Soberis/GKD_subscription',
@@ -26,3 +26,5 @@ export default defineGkdSubscription({
   globalGroups,
   apps: rawApps,
 });
+
+// Trigger release
